c0ae5ef53e7ad136bf1bae07d6055eb6a657b2c7
MCUVE <MauricioAntonioMartinez@outlook.com>
2020-04-16 00:06:49 -0500

    Updates

---
+++ precache-manifest.5f9e8d8f79cc4bb96ad254a0bb29983d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed0f6ee1daf333731f4dffc510582d2b",
+    "url": "/Tasker/index.html"
+  },
+  {
+    "revision": "51ff30124414ada3bf97",
+    "url": "/Tasker/static/css/main.d40b8da2.chunk.css"
+  },
+  {
+    "revision": "1fdb4afd2a3214543333",
+    "url": "/Tasker/static/js/2.e49c10b6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Tasker/static/js/2.e49c10b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51ff30124414ada3bf97",
+    "url": "/Tasker/static/js/main.61b0df52.chunk.js"
+  },
+  {
+    "revision": "5adbd2717af35f8b3dfe",
+    "url": "/Tasker/static/js/runtime-main.c1ead824.js"
+  }
+]);
